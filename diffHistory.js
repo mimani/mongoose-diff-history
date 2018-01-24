6508e522e905c40c7614ecb4291abcee0b05635c
Zachary J Rollyson <zach@izimobile.com>
2018-01-24 10:39:10 -0500

    Use Mongoose promise instead of async & callbacks.

--- diffHistory.js
+++ diffHistory.js
@@ -1,208 +1,136 @@
-const async = require('async');
+const pick = require('lodash.pick');
 const diffPatcher = require('jsondiffpatch').create();
 
 const History = require('./diffHistoryModel').model;
 
-const saveHistoryObject = function(history, callback) {
-  history.save(function(err) {
-    if (err) {
-      err.message = 'Mongo Error :' + err.message;
-    }
-    callback();
-  });
-};
-
-const saveDiffObject = function(currentObject, original, updated, user, reason, callback) {
+const saveDiffObject = (currentObject, original, updated, user, reason) => {
   const diff = diffPatcher.diff(
     JSON.parse(JSON.stringify(original)),
     JSON.parse(JSON.stringify(updated))
   );
 
-  if (!diff) return callback();
+  if (!diff) return;
 
-  History.findOne({
-    collectionName: currentObject.constructor.modelName,
-    collectionId: currentObject._id
-  })
+  const collectionId = currentObject._id;
+  const collectionName = currentObject.constructor.modelName;
+
+  return History.findOne({ collectionId, collectionName })
     .sort('-version')
-    .exec(function(err, lastHistory) {
-      if (err) {
-        err.message = 'Mongo Error :' + err.message;
-        return callback();
-      }
+    .then(lastHistory => {
       const history = new History({
-        collectionName: currentObject.constructor.modelName,
-        collectionId: currentObject._id,
-        diff: diff,
-        user: user,
-        reason: reason,
-        version: lastHistory ? lastHistory.version + 1 : 0
+        collectionId,
+        collectionName,
+        diff,
+        user,
+        reason,
+        version: lastHistory ? lastHistory.version + 1 : 0 // TODO: bad logic
       });
-      saveHistoryObject(history, callback);
+      return history.save();
     });
 };
 
-const saveDiffHistory = function(queryObject, currentObject, callback) {
-  currentObject.constructor.findOne({ _id: currentObject._id }, function(err, selfObject) {
+const saveDiffHistory = (queryObject, currentObject) => {
+  return currentObject.constructor.findOne({ _id: currentObject._id }).then(selfObject => {
     if (!selfObject) return;
 
-    const dbObject = {};
-    const updateParams = queryObject._update['$set']
-      ? queryObject._update['$set']
-      : queryObject._update;
-    Object.keys(updateParams).forEach(function(key) {
-      dbObject[key] = selfObject[key];
-    });
-    saveDiffObject(
+    const updateParams = queryObject._update['$set'] || queryObject._update;
+    const dbObject = pick(selfObject, Object.keys(updateParams));
+
+    return saveDiffObject(
       currentObject,
       dbObject,
       updateParams,
       queryObject.options.__user,
-      queryObject.options.__reason,
-      function() {
-        callback();
-      }
+      queryObject.options.__reason
     );
   });
 };
 
-const saveDiffs = function(self, next) {
-  const queryObject = self;
-  queryObject.find(queryObject._conditions, function(err, results) {
-    if (err) {
-      err.message = 'Mongo Error :' + err.message;
-      return next();
-    }
-    async.eachSeries(
-      results,
-      function(result, callback) {
-        if (err) {
-          err.message = 'Mongo Error :' + err.message;
-          return next();
-        }
-        saveDiffHistory(queryObject, result, callback);
-      },
-      function done() {
-        return next();
-      }
-    );
-  });
-};
+const saveDiffs = queryObject =>
+  queryObject
+    .find(queryObject._conditions)
+    .cursor()
+    .eachAsync(result => saveDiffHistory(queryObject, result)); // TODO: Not sure if cursor available here, swap to mapSeries if not.
 
-const getVersion = function(model, id, version, callback) {
-  model.findOne({ _id: id }, function(err, latest) {
-    if (err) {
-      console.error(err);
-      return callback(err, null);
-    }
-    History.find(
+const getVersion = (model, id, version) => {
+  return model.findOne({ _id: id }).then(latest => {
+    return History.find(
       {
         collectionName: model.modelName,
         collectionId: id,
         version: { $gte: parseInt(version, 10) }
       },
       { diff: 1, version: 1 },
-      { sort: '-version' },
-      function(err, histories) {
-        if (err) {
-          console.error(err);
-          return callback(err, null);
-        }
+      { sort: '-version' }
+    )
+      .cursor()
+      .eachAsync(history => {
+        // TODO: Not sure if cursor available here, swap to mapSeries if not.
         const object = latest ? latest : {};
-        async.each(
-          histories,
-          function(history, eachCallback) {
-            diffPatcher.unpatch(object, history.diff);
-            eachCallback();
-          },
-          function(err) {
-            if (err) {
-              console.error(err);
-              return callback(err, null);
-            }
-            callback(null, object);
-          }
-        );
-      }
-    );
+        return diffPatcher.unpatch(object, history.diff);
+      });
   });
 };
 
-const getHistories = function(modelName, id, expandableFields, callback) {
-  History.find({ collectionName: modelName, collectionId: id }, function(err, histories) {
-    if (err) {
-      console.error(err);
-      return callback(err, null);
-    }
-    async.map(
-      histories,
-      function(history, mapCallback) {
-        const changedValues = [];
-        const changedFields = [];
-        for (const key in history.diff) {
-          if (history.diff.hasOwnProperty(key)) {
-            if (expandableFields.indexOf(key) > -1) {
-              const oldValue = history.diff[key][0];
-              const newValue = history.diff[key][1];
-              changedValues.push(key + ' from ' + oldValue + ' to ' + newValue);
-            } else {
-              changedFields.push(key);
-            }
+const getHistories = (modelName, id, expandableFields) => {
+  return History.find({ collectionName: modelName, collectionId: id })
+    .cursor()
+    .eachAsync(history => {
+      const changedValues = [];
+      const changedFields = [];
+      for (const key in history.diff) {
+        if (history.diff.hasOwnProperty(key)) {
+          if (expandableFields.indexOf(key) > -1) {
+            const oldValue = history.diff[key][0];
+            const newValue = history.diff[key][1];
+            changedValues.push(key + ' from ' + oldValue + ' to ' + newValue);
+          } else {
+            changedFields.push(key);
           }
         }
-        const comment = 'modified ' + changedFields.concat(changedValues).join(', ');
-        return mapCallback(null, {
-          changedBy: history.user,
-          changedAt: history.createdAt,
-          updatedAt: history.updatedAt,
-          reason: history.reason,
-          comment: comment
-        });
-      },
-      function(err, output) {
-        if (err) {
-          console.error(err);
-          return callback(err, null);
-        }
-        return callback(null, output);
       }
-    );
-  });
+      const comment = 'modified ' + changedFields.concat(changedValues).join(', ');
+      return {
+        changedBy: history.user,
+        changedAt: history.createdAt,
+        updatedAt: history.updatedAt,
+        reason: history.reason,
+        comment: comment
+      };
+    });
 };
 
 const plugin = function lastModifiedPlugin(schema) {
   schema.pre('save', function(next) {
-    const self = this;
-    if (self.isNew) {
-      next();
-    } else {
-      self.constructor.findOne({ _id: self._id }, function(err, original) {
-        saveDiffObject(self, original, self, self.__user, self.__reason, function() {
-          next();
-        });
-      });
-    }
+    if (this.isNew) return next();
+    this.constructor
+      .findOne({ _id: this._id })
+      .then(original => saveDiffObject(this, original, this, this.__user, this.__reason))
+      .then(() => next())
+      .catch(next);
   });
 
   schema.pre('findOneAndUpdate', function(next) {
-    saveDiffs(this, function() {
-      next();
-    });
+    saveDiffs(this)
+      .then(() => next())
+      .catch(next);
   });
 
   schema.pre('update', function(next) {
-    saveDiffs(this, function() {
-      next();
-    });
+    saveDiffs(this)
+      .then(() => next())
+      .catch(next);
   });
 
   schema.pre('remove', function(next) {
-    saveDiffObject(this, this, {}, this.__user, this.__reason, function() {
-      next();
-    });
+    saveDiffObject(this, this, {}, this.__user, this.__reason)
+      .then(() => next())
+      .catch(next);
   });
 };
 
-module.exports.plugin = plugin;
-module.exports.getHistories = getHistories;
-module.exports.getVersion = getVersion;
+module.exports = {
+  plugin,
+  getVersion,
+  getHistories
+};
