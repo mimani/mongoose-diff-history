3b252fa0afab4ffe1d5cd22af53f827d1b06d9b1
Zachary J Rollyson <zach@izimobile.com>
2018-01-27 17:15:50 -0500

    Use 4 space indent.

--- diffHistory.js
+++ diffHistory.js
@@ -4,152 +4,152 @@ const diffPatcher = require('jsondiffpatch').create();
 const History = require('./diffHistoryModel').model;
 
 const isValidCb = cb => {
-  return cb && typeof cb === 'function';
+    return cb && typeof cb === 'function';
 };
 
 const saveDiffObject = (currentObject, original, updated, optsObj) => {
-  const { __user: user, __reason: reason } = optsObj || currentObject;
+    const { __user: user, __reason: reason } = optsObj || currentObject;
 
-  const diff = diffPatcher.diff(
-    JSON.parse(JSON.stringify(original)),
-    JSON.parse(JSON.stringify(updated))
-  );
+    const diff = diffPatcher.diff(
+        JSON.parse(JSON.stringify(original)),
+        JSON.parse(JSON.stringify(updated))
+    );
 
-  if (!diff) return;
+    if (!diff) return;
 
-  const collectionId = currentObject._id;
-  const collectionName = currentObject.constructor.modelName;
+    const collectionId = currentObject._id;
+    const collectionName = currentObject.constructor.modelName;
 
-  return History.findOne({ collectionId, collectionName })
-    .sort('-version')
-    .then(lastHistory => {
-      const history = new History({
-        collectionId,
-        collectionName,
-        diff,
-        user,
-        reason,
-        version: lastHistory ? lastHistory.version + 1 : 0
-      });
-      return history.save();
-    });
+    return History.findOne({ collectionId, collectionName })
+        .sort('-version')
+        .then(lastHistory => {
+            const history = new History({
+                collectionId,
+                collectionName,
+                diff,
+                user,
+                reason,
+                version: lastHistory ? lastHistory.version + 1 : 0
+            });
+            return history.save();
+        });
 };
 
 const saveDiffHistory = (queryObject, currentObject) => {
-  return currentObject.constructor.findOne({ _id: currentObject._id }).then(selfObject => {
-    if (!selfObject) return;
+    return currentObject.constructor.findOne({ _id: currentObject._id }).then(selfObject => {
+        if (!selfObject) return;
 
-    const updateParams = queryObject._update['$set'] || queryObject._update;
-    const dbObject = pick(selfObject, Object.keys(updateParams));
+        const updateParams = queryObject._update['$set'] || queryObject._update;
+        const dbObject = pick(selfObject, Object.keys(updateParams));
 
-    return saveDiffObject(currentObject, dbObject, updateParams, queryObject.options);
-  });
+        return saveDiffObject(currentObject, dbObject, updateParams, queryObject.options);
+    });
 };
 
 const saveDiffs = queryObject =>
-  queryObject
-    .find(queryObject._conditions)
-    .cursor()
-    .eachAsync(result => saveDiffHistory(queryObject, result));
+    queryObject
+        .find(queryObject._conditions)
+        .cursor()
+        .eachAsync(result => saveDiffHistory(queryObject, result));
 
 const getVersion = (model, id, version, cb) => {
-  return model
-    .findById(id)
-    .then(latest => {
-      latest = latest || {};
-      return History.find(
-        {
-          collectionName: model.modelName,
-          collectionId: id,
-          version: { $gte: parseInt(version, 10) }
-        },
-        { diff: 1, version: 1 },
-        { sort: '-version' }
-      )
-        .lean()
-        .cursor()
-        .eachAsync(history => {
-          diffPatcher.unpatch(latest, history.diff);
+    return model
+        .findById(id)
+        .then(latest => {
+            latest = latest || {};
+            return History.find(
+                {
+                    collectionName: model.modelName,
+                    collectionId: id,
+                    version: { $gte: parseInt(version, 10) }
+                },
+                { diff: 1, version: 1 },
+                { sort: '-version' }
+            )
+                .lean()
+                .cursor()
+                .eachAsync(history => {
+                    diffPatcher.unpatch(latest, history.diff);
+                })
+                .then(() => {
+                    if (isValidCb(cb)) return cb(null, latest);
+                    return latest;
+                });
         })
-        .then(() => {
-          if (isValidCb(cb)) return cb(null, latest);
-          return latest;
+        .catch(err => {
+            if (isValidCb(cb)) return cb(err, null);
+            throw err;
         });
-    })
-    .catch(err => {
-      if (isValidCb(cb)) return cb(err, null);
-      throw err;
-    });
 };
 
 const getHistories = (modelName, id, expandableFields, cb) => {
-  const histories = [];
-  return History.find({ collectionName: modelName, collectionId: id })
-    .cursor()
-    .eachAsync(history => {
-      const changedValues = [];
-      const changedFields = [];
-      for (const key in history.diff) {
-        if (history.diff.hasOwnProperty(key)) {
-          if (expandableFields.indexOf(key) > -1) {
-            const oldValue = history.diff[key][0];
-            const newValue = history.diff[key][1];
-            changedValues.push(key + ' from ' + oldValue + ' to ' + newValue);
-          } else {
-            changedFields.push(key);
-          }
-        }
-      }
-      const comment = 'modified ' + changedFields.concat(changedValues).join(', ');
-      histories.push({
-        changedBy: history.user,
-        changedAt: history.createdAt,
-        updatedAt: history.updatedAt,
-        reason: history.reason,
-        comment: comment
-      });
-    })
-    .then(() => {
-      if (isValidCb(cb)) return cb(null, histories);
-      return histories;
-    })
-    .catch(err => {
-      if (isValidCb(cb)) return cb(err, null);
-      throw err;
-    });
+    const histories = [];
+    return History.find({ collectionName: modelName, collectionId: id })
+        .cursor()
+        .eachAsync(history => {
+            const changedValues = [];
+            const changedFields = [];
+            for (const key in history.diff) {
+                if (history.diff.hasOwnProperty(key)) {
+                    if (expandableFields.indexOf(key) > -1) {
+                        const oldValue = history.diff[key][0];
+                        const newValue = history.diff[key][1];
+                        changedValues.push(key + ' from ' + oldValue + ' to ' + newValue);
+                    } else {
+                        changedFields.push(key);
+                    }
+                }
+            }
+            const comment = 'modified ' + changedFields.concat(changedValues).join(', ');
+            histories.push({
+                changedBy: history.user,
+                changedAt: history.createdAt,
+                updatedAt: history.updatedAt,
+                reason: history.reason,
+                comment: comment
+            });
+        })
+        .then(() => {
+            if (isValidCb(cb)) return cb(null, histories);
+            return histories;
+        })
+        .catch(err => {
+            if (isValidCb(cb)) return cb(err, null);
+            throw err;
+        });
 };
 
 const plugin = function lastModifiedPlugin(schema) {
-  schema.pre('save', function(next) {
-    if (this.isNew) return next();
-    this.constructor
-      .findOne({ _id: this._id })
-      .then(original => saveDiffObject(this, original, this))
-      .then(() => next())
-      .catch(next);
-  });
+    schema.pre('save', function (next) {
+        if (this.isNew) return next();
+        this.constructor
+            .findOne({ _id: this._id })
+            .then(original => saveDiffObject(this, original, this))
+            .then(() => next())
+            .catch(next);
+    });
 
-  schema.pre('findOneAndUpdate', function(next) {
-    saveDiffs(this)
-      .then(() => next())
-      .catch(next);
-  });
+    schema.pre('findOneAndUpdate', function (next) {
+        saveDiffs(this)
+            .then(() => next())
+            .catch(next);
+    });
 
-  schema.pre('update', function(next) {
-    saveDiffs(this)
-      .then(() => next())
-      .catch(next);
-  });
+    schema.pre('update', function (next) {
+        saveDiffs(this)
+            .then(() => next())
+            .catch(next);
+    });
 
-  schema.pre('remove', function(next) {
-    saveDiffObject(this, this, {})
-      .then(() => next())
-      .catch(next);
-  });
+    schema.pre('remove', function (next) {
+        saveDiffObject(this, this, {})
+            .then(() => next())
+            .catch(next);
+    });
 };
 
 module.exports = {
-  plugin,
-  getVersion,
-  getHistories
+    plugin,
+    getVersion,
+    getHistories
 };
--- diffHistoryModel.js
+++ diffHistoryModel.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const historySchema = new Schema(
-  {
-    collectionName: String,
-    collectionId: Schema.Types.ObjectId,
-    diff: {},
-    user: {},
-    reason: String,
-    version: { type: Number, min: 0 }
-  },
-  {
-    timestamps: true
-  }
+    {
+        collectionName: String,
+        collectionId: Schema.Types.ObjectId,
+        diff: {},
+        user: {},
+        reason: String,
+        version: { type: Number, min: 0 }
+    },
+    {
+        timestamps: true
+    }
 );
 
 module.exports = { model: mongoose.model('History', historySchema) };
